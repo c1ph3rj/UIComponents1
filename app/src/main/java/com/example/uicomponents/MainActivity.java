4e59999f8b3f5e12ca59b418fc18fb2fd02f76d2
rajasekar20jan <rs20jan1998s@gmail.com>
2022-09-30 00:41:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uicomponents/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.uicomponents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView signUp= (TextView) findViewById(R.id.textview1);
+        TextView details=(TextView) findViewById(R.id.textview2);
+        EditText userName=(EditText) findViewById(R.id.textBox);
+        Button btn=(Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+
+
+        });
+    }
+
+
+
+}
